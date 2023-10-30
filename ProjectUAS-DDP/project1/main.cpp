00fa7c6c839866be7a952e090a191572cd6213f1
febrina1602 <112796775+febrina1602@users.noreply.github.com>
2023-10-30 22:38:12 +0700

    Add files via upload

---
+++ ProjectUAS-DDP/project1/main.cpp
@@ -0,0 +1,17 @@
+#include "project.h"
+
+int main (){
+	system("color B");
+	system("cls");
+
+	halamanAwal();
+	system("cls");
+
+	//verifikasi data user
+	opsiLogin();
+
+	
+
+	linexy(0,0);
+	return 0;
+}
